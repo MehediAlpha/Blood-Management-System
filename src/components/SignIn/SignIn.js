9f9d2d61a51c3048e873dc555c4886b9df9f2752
S. M. Mehedi <syedmehedi998@gmail.com>
2021-05-01 17:40:07 +0600

    Case

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -7,7 +7,7 @@ const SignIn = ({onRouteChange}) => {
 		<main className="pa5 dib br4 mv3 bg-white css">
 		  <div className="measure center">
 		    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-		      <legend className="f2 fw6 ph0 mh0">SIGN IN</legend>
+		      <legend className="f2 fw6 ph0 mh0">Sign In</legend>
 		      <div className="mt3">
 		        <label className="tl db fw6 lh-copy f4" htmlFor="email-address">Email</label>
 		        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
